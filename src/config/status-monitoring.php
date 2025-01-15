f878be30db654b2cafaf2c23a33eb012459ec2cc
Segun Olaiya <vadeshayo@gmail.com>
2025-01-15 13:06:40 +0100

    Nits

--- src/config/status-monitoring.php
+++ src/config/status-monitoring.php
@@ -5,7 +5,7 @@ use App\Jobs\Policies\Pages\ComprehensiveHealth\ComprehensiveHealthManagerJob;
 use App\Jobs\Policies\Pages\GeneralHealthJob;
 
 return [
-    'auth-token' => env('API_AUTH_TOKEN', '4a63bb7c-1e14-4cf0-ba31-ddcddf79b277'),
+    'auth-token' => env('API_AUTH_TOKEN', ''),
     'policies' => [
         PolicyType::PAGES_HEALTH_GENERAL->value => GeneralHealthJob::class,
         PolicyType::PAGES_HEALTH_COMPREHENSIVE->value => ComprehensiveHealthManagerJob::class,
